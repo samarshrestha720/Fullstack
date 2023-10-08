1cadcb322eaf1f7d08d30109e742f3ea9d8fe418
Samar Shrestha <66566474+samarshrestha720@users.noreply.github.com>
2023-10-08 11:27:22 +0545

    Initial commit

---
+++ part1/src/App.jsx
@@ -0,0 +1,30 @@
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const friends = ["Peter", "Maya"];
+
+  return (
+    <div>
+      <p>{friends[0]}</p>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+  );
+};
+
+export default App;
